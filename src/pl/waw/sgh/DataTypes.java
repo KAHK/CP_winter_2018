b22f1c52ff721951e5f1e82a4a6661b18f9e758b
KAHK <kaaghika@gmail.com>
2018-10-17 11:22:07 +0200

    Playing with Datatypes

---
+++ src/pl/waw/sgh/DataTypes.java
@@ -0,0 +1,43 @@
+package pl.waw.sgh;
+
+public class DataTypes {
+
+      /*
+      This is a comment, Ctrl + Shift + / is a shortcut to make text a comment
+       Press again to uncomment
+       */
+
+    //pvsm - this is an one-line comment I suppose
+    public static void main(String[] args) {
+        byte b1 = 5;
+        byte b2 = 120;
+ /*
+       int i1 = 1434646464;
+        int i2 = 985245454
+                int i3 = i1 + i2;
+  */
+        long i1 = 165419849;
+        long i2 = 4165498498;
+        long i3 = i1 + i2
+        //sout
+        // Python : str(i3)
+        System.out.println("i3: " + i3);
+        int a = 5;
+        int b, bb;
+        b = a++;
+        a = 5;
+        bb = ++a;
+
+        // Ctrl + D to copy the line
+        System.out.println("b=" + b);
+        System.out.println("bb=" + bb);
+
+        double d1 = 2;
+        double d2 = 3;
+
+        double d3 = Math.pow(d1, d2);
+
+        System.out.println("d3=" + d3);
+
+    }
+}
