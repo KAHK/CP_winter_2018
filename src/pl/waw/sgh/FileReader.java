7a077d55a54102019c43b42b6b16ef95a44c8b80
KAHK <kaaghika@gmail.com>
2018-11-14 10:13:52 +0100

    trying to fix FileReader

--- src/pl/waw/sgh/FileReader.java
+++ src/pl/waw/sgh/FileReader.java
@@ -6,16 +6,16 @@ import java.util.Scanner;
 
 public class FileReader {
     public static void main(String[] args) throws FileNotFoundException {
-        //File inFile = new File("h:\\mytxtfile.txt");
-        File inFile = new File("h:/mytxtfile.txt");
-        //File inFile = new File("h:" + File.separator +"/mytxtfile.txt");
+        File inFile = new File("h:\\mytxtfile.txt");
+        //File inFile = new File("H:\\CP_winter_2018\\mytxtfile.txt");
+        //File inFile = new File("h:\\CP_winter_2018" + File.separator +"\\mytxtfile.txt");
         // double backlash or slash or "h:" + File.separator + "/rest of the path"
 
         Scanner scanner = new Scanner(inFile);
         int i = 1;
         while (scanner.hasNext()) {
             String line = scanner.nextLine();
-            String[] elems = line.split(";")
+            String[] elems = line.split(";");
             System.out.println(i + ": " + line);
 
             i++;
