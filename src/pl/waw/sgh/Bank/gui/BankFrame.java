b3c9d35d4de485e4d1e1a7031d607b780a8c4e1b
KAHK <kaaghika@gmail.com>
2018-12-19 10:44:24 +0100

    Monika's GUI + Bank Frame

---
+++ src/pl/waw/sgh/Bank/gui/BankFrame.java
@@ -0,0 +1,13 @@
+package pl.waw.sgh.Bank.gui;
+
+import javax.swing.*;
+
+public class BankFrame {
+
+    public static void main(String[] args) {
+        JFrame bankFrame = new JFrame("Bank App");
+        bankFrame.setSize(500, 500);
+        bankFrame.setVisible(true);
+        bankFrame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+    }
+}
